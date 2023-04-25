0e3ed1907890211b3fa02a6039163ecca2612385
Spacehuhn <github@spacehuhn.de>
2023-04-25 18:37:32 +0200

    Images & empty pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,7 +67,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'about',
             position: 'left',
             label: 'Docs',
           },
